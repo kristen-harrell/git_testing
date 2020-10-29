1e66b1069015613c778714ccdb1f037e1b0c40ae
Kristen Harrell <harrell.kristen@gmail.com>
2020-10-29 10:34:52 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace github_testing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("adding a line");
+            Console.WriteLine("adding another interesting line");
+
+        }
+    }
+}
